f642c42c21116b83e13dd77b9a3373a9eb4fd3a3
tapankumar003 <tapankumar29808@gmail.com>
2024-11-14 08:39:37 +0530

    java servlet project added

---
+++ backend_java/DemoProject/src/main/java/com/HelloServlet.java
@@ -0,0 +1,20 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		String nm = request.getParameter("name");
+		out.print("<h1>hello,"+nm+"</h1>");
+	}
+
+}
